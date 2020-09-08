5088c2fc34de40b42e75529eb73b4c5b0e02d0f6
v0h00q1 <vageesh.hegde@walmartlabs.com>
2020-09-08 09:04:30 +0530

    Added validations

--- src/main/java/com/intuit/interview/tictactoe/utils/Constants.java
+++ src/main/java/com/intuit/interview/tictactoe/utils/Constants.java
@@ -4,7 +4,6 @@ public class Constants
 {
 	public static final String GAME_STARTED = "Game begun successfully. Make your first Move";
 	public static final String GAME_START_FAILED = "Game could not be started";
-	public static final String NO_GAME_ID = "NO_GAME_ID";
 
 	public static final String MARK_OK = "Mark was successful. Computer Made it's move. Make your next move ";
 	public static final String PLAYER_MARK_NOK = "Player mark was not successful";
@@ -14,4 +13,6 @@ public class Constants
 	public static final String DRAW = "It's a draw!";
 
 	public static final String NO_GAME_ERROR = "No Game with given ID exists";
+
+	public static final String VALIDATION_ERROR = "One/more required attributes missing in the payload/request";
 }
